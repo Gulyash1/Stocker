d9663c5266fee0ad5d56316febab162afd46f7b6
Gulyash1 <97738530+Gulyash1@users.noreply.github.com>
2022-01-14 15:37:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -61,24 +61,7 @@ def apr_change(pandas_series_input):
 
 price_data['change'] = apr_change(price_data['4. close'])
 market_data['change'] = apr_change(market_data['4. close'])
-#
-# price_data_filtered = price_data[end_date:start_date]
-# market_data_filtered = market_data[end_date:start_date]
-# stock_market_correlation = price_data_filtered['change'].corr(market_data_filtered['change'], method='pearson')
-#
-# treasury_yield = quandl.get("FRED/TB3MS", start_date=start_date, end_date=end_date)
-# rfr = treasury_yield['Value'].mean()
-#
-# stock_volatility = price_data_filtered['change'].std()
-# market_volatilidy = market_data_filtered['change'].std()
-# stock_excess_return = price_data_filtered['change'].mean() - rfr
-# market_excess_return = market_data_filtered['change'].mean() - rfr
-# beta = stock_market_correlation * stock_volatility / market_volatilidy
-# alpha = stock_excess_return - beta * market_excess_return
-# sharpe = stock_excess_return / stock_volatility
-# metrics_df = pd.DataFrame(
-#     data={'mkt correlation': [stock_market_correlation], 'alpha': [alpha], 'beta': [beta], 'Sharpe ratio': [sharpe]})
-# metrics_df.index = [ticker]
+
 
 st.markdown(md_chart_1)
 st.line_chart(price_data['4. close'])
@@ -86,102 +69,105 @@ st.dataframe(price_data['4. close'])
 st.markdown(md_chart_2)
 st.line_chart(price_data['change'])
 st.dataframe(price_data)
-#st.table(metrics_df)
 
 
 
-df = ts.get_daily(symbol=ticker, outputsize='full')[0]
-data = pu.data_preparation(df)
+with st.spinner('Подождите. Выполняются вычисления'):
+    df = ts.get_daily(symbol=ticker, outputsize='full')[0]
+    data = pu.data_preparation(df)
 
     # разбиение на тестовую и обучающую выборки
-count_step_for_learn = 350
-X_train, X_test, y_train, y_test, count_step_for_learn = pu.scaled(data, 0.7, count_step_for_learn)
+    count_step_for_learn = 350
+    X_train, X_test, y_train, y_test, count_step_for_learn = pu.scaled(data, 0.7, count_step_for_learn)
 
-    # обучение модели
-if not os.path.isdir('LSTM2_model') or count_step_for_learn != 350:
-    pu.fit_model(X_train, y_train)
+        # обучение модели
+    if not os.path.isdir('LSTM2_model') or count_step_for_learn != 350:
+        pu.fit_model(X_train, y_train)
 
     # загрузка модели
-model_load = tf.keras.models.load_model('LSTM2_model')
+    model_load = tf.keras.models.load_model('LSTM2_model')
 
-    # предсказание для тестовой выборки
-closing_price = model_load.predict(X_test)
-
-    # инверсия нормализации
-with open("scaler.pickle", "rb") as input_file:
-    scaler = pickle.load(input_file)
-closing_price = scaler.inverse_transform(closing_price)
-
-    # график прогнозов
-train_data = data[:-len(closing_price)]
-valid_data = data[-len(closing_price):]
-valid_data = valid_data.assign(Predictions=closing_price)
-pu.prediction_plot(train_data['4. close'], valid_data['4. close'], valid_data['Predictions'])
-
-    # создаем новый фрейм с прошлыми прогнозами
-pred_data = pd.DataFrame(columns=['Forecast'])
-pred_data.loc[len(data)] = data.values[-1][0]
-old_index = len(data)
-
-# n = 50
-n = st.slider('Количество дней прогноза:', 0, 350, 50)
-print('Прогноз выбранного количества значений: ', n)
-forecast_n = []
-vect_pred = valid_data['4. close'].values
-
-for i in range(n):
-    vect_pred = vect_pred[-350:]
-    # подгонка подпространств
-    v_pred = []
-    for v in vect_pred:
-        v_pred.append([v])
-    scaled_pred = scaler.fit_transform(v_pred)
-    scaled_pred = np.array([scaled_pred])
-
-        # прогноз одного элемента
-    one_step_pred = model_load.predict(scaled_pred)
+        # предсказание для тестовой выборки
+    closing_price = model_load.predict(X_test)
 
         # инверсия нормализации
     with open("scaler.pickle", "rb") as input_file:
         scaler = pickle.load(input_file)
-    one_step_pred = scaler.inverse_transform(one_step_pred)[0][0]
+    closing_price = scaler.inverse_transform(closing_price)
 
-    print('Прогноз ' + str(i + 1) + ': ' + str(one_step_pred))
-    forecast_n.append(one_step_pred)
-    index = old_index + i + 1
-    pred_data.loc[index] = one_step_pred
+        # график прогнозов
+    train_data = data[:-len(closing_price)]
+    valid_data = data[-len(closing_price):]
+    valid_data = valid_data.assign(Predictions=closing_price)
+    pu.prediction_plot(train_data['4. close'], valid_data['4. close'], valid_data['Predictions'])
 
-     # добавляем прогноз в вектор
-    vect_pred = list(vect_pred)
-    vect_pred.append(one_step_pred)
-    vect_pred = np.array(vect_pred)
+        # создаем новый фрейм с прошлыми прогнозами
+    pred_data = pd.DataFrame(columns=['Forecast'])
+    pred_data.loc[len(data)] = data.values[-1][0]
+    old_index = len(data)
+
+    # n = 50
+    n = st.slider('Количество дней прогноза:', 0, 350, 50)
+    print('Прогноз выбранного количества значений: ', n)
+    forecast_n = []
+    vect_pred = valid_data['4. close'].values
+
+    for i in range(n):
+        vect_pred = vect_pred[-350:]
+        # подгонка подпространств
+        v_pred = []
+        for v in vect_pred:
+            v_pred.append([v])
+        scaled_pred = scaler.fit_transform(v_pred)
+        scaled_pred = np.array([scaled_pred])
+
+            # прогноз одного элемента
+        one_step_pred = model_load.predict(scaled_pred)
+
+            # инверсия нормализации
+        with open("scaler.pickle", "rb") as input_file:
+            scaler = pickle.load(input_file)
+        one_step_pred = scaler.inverse_transform(one_step_pred)[0][0]
+
+        print('Прогноз ' + str(i + 1) + ': ' + str(one_step_pred))
+        forecast_n.append(one_step_pred)
+        index = old_index + i + 1
+        pred_data.loc[index] = one_step_pred
+
+         # добавляем прогноз в вектор
+        vect_pred = list(vect_pred)
+        vect_pred.append(one_step_pred)
+        vect_pred = np.array(vect_pred)
 
 
-plotdata = pd.DataFrame({"Close": data['4. close'],
-                         "Forecast": pred_data['Forecast']})
+    plotdata = pd.DataFrame({"Close": data['4. close'],
+                             "Forecast": pred_data['Forecast']})
 
-st.markdown(md_chart_3)
-st.line_chart(plotdata)
+    #st.line_chart(plotdata)
 
-plot_1 = plotdata
-date_list = list(df['date'])
-end_date = dt.date.today()
-rp = list([end_date + dt.timedelta(days=x) for x in range(n)])
-date_list = date_list + rp
-date_list = pd.DataFrame({'Date': date_list})
-date_list = date_list.sort_values(by='Date')
-date_list = date_list.reset_index(drop=True)
-plot_1['Date'] = date_list['Date']
-plot_1.reset_index(level=0, drop=True)
-plot_1 = plot_1.set_index('Date')
-
+    plot_1 = plotdata
+    date_list = list(df['date'])
+    end_date = dt.date.today()
+    rp = list([end_date + dt.timedelta(days=x) for x in range(n)])
+    date_list = date_list + rp
+    date_list = pd.DataFrame({'Date': date_list})
+    date_list = date_list.sort_values(by='Date')
+    date_list = date_list.reset_index(drop=True)
+    plot_1['Date'] = date_list['Date']
+    plot_1.reset_index(level=0, drop=True)
+    plot_1 = plot_1.set_index('Date')
 
 
 
-#st.line_chart(data['4. close'])
-#st.line_chart(ha)
-st.line_chart(plot_1)
-st.dataframe(plot_1)
+
+    #st.line_chart(data['4. close'])
+    #st.line_chart(ha)
+    st.markdown(md_chart_3)
+    st.line_chart(plot_1)
+    st.dataframe(plot_1)
+    time.sleep(5)
+st.success('Готово!')
+time.sleep(5)
 
 
 
